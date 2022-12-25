b9f03126fcd3428a858ac3a9ff72eb2e2f8c3840
Blacklight <bilalabdlhamid6@gmail.com>
2022-12-25 12:59:34 +0100

    Revert "fbx package"
    This reverts commit b3bb5346dc904fd2f980f7a9a79e33271e8b0512, reversing
    changes made to 449ded0d868ebd9ccd9effd75ffb2934d3e0e4da.

--- Creepy Christmas/Assets/PointCounter.cs
+++ Creepy Christmas/Assets/PointCounter.cs
@@ -8,8 +8,7 @@ public class PointCounter : MonoBehaviour
     public float points;
     [SerializeField] TMP_Text pointsText;
 
-    [SerializeField] GameObject secretroomwall;
-    [SerializeField] bool secretroomopen;
+    [SerializeField] 
     public void AddPoints(float points)
     {
         this.points += points;
@@ -18,11 +17,9 @@ public class PointCounter : MonoBehaviour
     {
         pointsText.text = points.ToString();
 
-        if(points > 10 && secretroomopen == false)
+        if(points > 20)
         {
-            secretroomopen = true;
-            Destroy(secretroomwall);
-            ScreenMessager.Instance.SendScreenMessage("A secret room has been opened!", Color.green);
+
         }
     }
 }
